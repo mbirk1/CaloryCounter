999d0118bf15c68119433d05c0bb7e821049fe24
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2024-07-06 00:29:05 +0200

    Add React and add first navigation labels

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,7 @@
+export default function dashboard() {
+    return (
+        <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        )
+}
---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,11 @@
+import '../styles/Navigation.css';
+
+export default function Navigation() {
+    return (
+        <div className="bar">
+            <label>Dashboard</label>
+            <label>Food</label>
+            <label>Recipe</label>
+        </div>
+       );
+}
