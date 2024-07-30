54fc366f93987196f2ba5678f9df4eebfe8ed3d6
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2024-07-30 22:12:30 +0200

    Add new tests

--- backend/src/test/java/de/birk/calory/adapter/primary/RecipeRestControllerTest.java
+++ backend/src/test/java/de/birk/calory/adapter/primary/RecipeRestControllerTest.java
@@ -60,4 +60,50 @@ public class RecipeRestControllerTest extends AbstractTestBase {
         .andExpect(jsonPath("$.foods[0].calory").value(1312))
         .andExpect(jsonPath("$.foods[0].grams").value(100));
   }
+
+  @Test
+  @DisplayName("Tries to get a non existing Recipe")
+  public void getRecipeAndCatchExceptionTest() throws Exception {
+    this.mockMvc.perform(
+                    get("/recipe/{id}", UUID.randomUUID())
+            )
+            .andExpect(status().isNotFound());
+  }
+
+  @Test
+  @DisplayName("create one and get all Recipes")
+  public void createOneGetAllRecipeTest() throws Exception {
+    String foodContent = readResourceAsString("/http-bodies/createFood.json");
+
+    MvcResult foodResult = mockMvc.perform(
+            post("/food")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(foodContent)
+    ).andReturn();
+
+    ReadContext context = asJson(foodResult);
+    String foodId = context.read("$.uuid");
+
+    String content = readResourceAsString("/http-bodies/createRecipe.json");
+    content = content.replace("PLACEHOLDER", foodId);
+
+    MvcResult mvcResult = mockMvc.perform(
+            post("/recipe")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(content)
+    ).andReturn();
+
+    ReadContext readContext = asJson(mvcResult);
+    String id = readContext.read("$.id");
+
+    this.mockMvc.perform(
+                    get("/recipe", UUID.fromString(id))
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].id").value(id))
+            .andExpect(jsonPath("$[0].name").value("recipe"))
+            .andExpect(jsonPath("$[0].foods[0].name").value("food"))
+            .andExpect(jsonPath("$[0].foods[0].calory").value(1312))
+            .andExpect(jsonPath("$[0].foods[0].grams").value(100));
+  }
 }
