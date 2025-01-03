4285f5c1908e1abb517ed36fddc731075a2510e4
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2025-01-03 22:48:56 +0100

    change workflow

---
+++ backend/src/main/java/de/birk/calory/adapter/primary/FallbackController.java
@@ -0,0 +1,12 @@
+package de.birk.calory.adapter.primary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+  @GetMapping(value = "/{path:[^\\.]*}")
+  public String redirect() {
+      return "forward:/index.html";
+  }
+}
