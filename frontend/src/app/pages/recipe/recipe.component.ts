7c9a09da2856d67a8d9e335246491c49f190d5f7
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2025-09-12 23:39:01 +0200

    35: fix loading food bug

--- frontend/src/app/pages/recipe/recipe.component.ts
+++ frontend/src/app/pages/recipe/recipe.component.ts
@@ -4,6 +4,7 @@ import { RecipeService } from '../../services/recipe.service'
 import { RecipeTableComponent } from '../../components/recipe-table/recipe-table.component'
 import { Dialog } from '@angular/cdk/dialog'
 import { AddRecipeDialogComponent } from '../../components/dialogs/add-recipe-dialog/add-recipe-dialog.component'
+import { FoodStore } from '../../api/stores/food.store'
 
 @Component({
   selector: 'app-recipe',
@@ -13,10 +14,12 @@ import { AddRecipeDialogComponent } from '../../components/dialogs/add-recipe-di
 })
 export class RecipeComponent {
   recipeService: RecipeService = inject(RecipeService)
+  foodStore: FoodStore = inject(FoodStore)
 
   constructor(private dialog: Dialog) {}
 
   handleRecipeDialog() {
+    this.foodStore.load()
     const dialogRef = this.dialog.open(AddRecipeDialogComponent, {})
   }
 }
